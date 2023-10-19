87e84dc36fab96289d2c5132142795f4db0931bf
irawandwi0803 <dwiirawan0803@gmail.com>
2023-10-19 17:09:36 +0700

    commit pertama

--- app.js
+++ app.js
@@ -1,34 +1,45 @@
 const readline = require('readline');
 const validator = require('validator');
-const {default: isEmail} = require('validator/lib/isEmail');
-const {default: isMobilePhone} = require('validator/lib/isMobilePhone');
 
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-    rl.question("what is your name? ", (nama) => {
-        rl.question("your phone number is? ", (noTelp) => {
-            rl.question("your email is? ", (email) => {
-            
-                // validasi email
-            if(email, noTelp){
-                if(!validator.isEmail(email)){
-                    console.log('Email Tidak Valid');
-                }
-                if(!validator.isMobilePhone(noTelp, 'id-ID')){
-                    console.log('No Telp Tidak Valid')
-                    
-                }else {
-                    console.log('My name is',nama + ', Your phone number ',noTelp + ', Your email is ',email);
-                    rl.close()
-                }
-                return false;
-            }
-                // console.log('Data Berhasil Ditambahkan');
-                // rl.close()
-            });
-        });
+function main() {
+    function getNama() {
+    rl.question("Masukkan nama: ", (nama) => {
+        getEmail(nama);
     });
+}
 
+function getEmail(nama) {
+    rl.question('Masukkan alamat email: ', (email) => {
+    if (validator.isEmail(email)) {
+        console.log('Alamat email valid: ' + email);
+        getNoTelp(nama, email);
+    }else {
+            console.log('Alamat email tidak valid. Silakan coba lagi.');
+            getEmail(nama);
+        }
+    });
+}
+
+function getNoTelp(nama, email) {
+    rl.question('Masukkan nomor telepon: ', (noTelp) => {
+    if (validator.isMobilePhone(noTelp, 'id-ID')) {
+    console.log('Nomor telepon valid: ' + noTelp);
+    console.log(`Nama kamu adalah ${nama}, Email kamu adalah ${email}, Dan nomor handphone kamu adalah ${noTelp}`)
+    rl.close();
+    } else {
+            console.log('Nomor telepon tidak valid. Silakan coba lagi.');
+            getNoTelp(nama, email);
+        }
+    });
+}
+
+getNama()
+
+}
+// Memulai app
+main();
