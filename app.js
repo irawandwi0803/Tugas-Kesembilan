fafc1d69fb72f4fd28b33e69824c4edd709ac14d
irawandwi0803 <dwiirawan0803@gmail.com>
2023-10-19 15:03:20 +0700

    commit pertama

---
+++ app.js
@@ -0,0 +1,34 @@
+const readline = require('readline');
+const validator = require('validator');
+const {default: isEmail} = require('validator/lib/isEmail');
+const {default: isMobilePhone} = require('validator/lib/isMobilePhone');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+    rl.question("what is your name? ", (nama) => {
+        rl.question("your phone number is? ", (noTelp) => {
+            rl.question("your email is? ", (email) => {
+            
+                // validasi email
+            if(email, noTelp){
+                if(!validator.isEmail(email)){
+                    console.log('Email Tidak Valid');
+                }
+                if(!validator.isMobilePhone(noTelp, 'id-ID')){
+                    console.log('No Telp Tidak Valid')
+                    
+                }else {
+                    console.log('Data Berhasil Ditambahkan');
+                    rl.close()
+                }
+                return false;
+            }
+                // console.log('Data Berhasil Ditambahkan');
+                // rl.close()
+            });
+        });
+    });
+
